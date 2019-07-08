9ad58b9fa9740e79abbf8b985d61047a89afaccc
cabbageDev <cbhong@ucsc.edu>
2019-07-08 05:15:45 -0700

    pa1 updates

--- pa1/Lex.c
+++ pa1/Lex.c
@@ -14,7 +14,7 @@
 
 int main(int argc, char* argv[]){
 	int count = 0;
-	FILE *in, *out;
+	FILE *input, *output;
 	char line[MAX_LEN];
 
 	//check args
@@ -22,32 +22,61 @@ int main(int argc, char* argv[]){
 		printf("Usage: %s <input file> <output file>\n", argv[0]);
 		exit(1);
 	}
-	in = fopen(argv[1], "r");
-    out = fopen(argv[2], "w");
-	if(in==NULL){
+	input = fopen(argv[1], "r");
+    output = fopen(argv[2], "w");
+	if(input==NULL){
 		printf( "Unable to open file %s for reading\n", argv[1]);
 		exit(1);
 	}
-	if(out==NULL){
+	if(output==NULL){
 		printf("Unable to open file %s for writing\n", argv[2]);
 		exit(1);
 	}
 
-	while( fgets(line, MAX_LEN, in) != NULL)  {
+	while( fgets(line, MAX_LEN, input) != NULL)  {
 		count++;
 	}
 
-	rewind(in);
 	char lines[count - 1][MAX_LEN];
-
-	while(fgets(line, MAX_LEN, in) != NULL) {
-		strcpy(lines[N], line);
+	int i = 0;
+	rewind(input);
+	
+	while(fgets(line, MAX_LEN, input) != NULL) {
+		strcpy(lines[i], line);
+		i++;
 	}
 
 	List list = newList();
 
 	append(list, 0);
-	
-	
 
+	for(int j = 1; j < count; ++j){
+		char *tmp = lines[j];
+		int i =  j - 1;
+		moveBack(list);
+		while(i >= 0 && strcmp(tmp, lines[get(list)]) <= 0 ){
+			--i;
+			movePrev(list);
+		}
+		if (index(list) >= 0){
+			insertAfter(list, j);
+		} else{
+			prepend(list, j);
+		}
+	}
+
+
+	moveFront(list);
+
+	while(index(list)>=0){
+		fprintf(output, "%s", lines[get(list)]);
+		moveNext(list);
+	}
+
+	fclose(input);
+	fclose(output);
+
+	freeList(&list);
+
+	return 0;
 }
